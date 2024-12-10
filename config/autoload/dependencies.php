a225e6b19daef975b0d616b7dd17496d5c70a5ce
arthurisvi <spfcarthur1@gmail.com>
2024-12-10 14:47:29 -0300

    chore: resolve dependencias do DI para uso de classes concretas

--- config/autoload/dependencies.php
+++ config/autoload/dependencies.php
@@ -9,5 +9,19 @@ declare(strict_types=1);
  * @contact  group@hyperf.io
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
+
+use Modules\Transaction\Repository\TransactionRepositoryInterface;
+use Modules\Transaction\Repository\TransactionRepository;
+use Modules\User\Repository\UserRepositoryInterface;
+use Modules\User\Repository\UserRepository;
+use Modules\Wallet\Repository\WalletRepositoryInterface;
+use Modules\Wallet\Repository\WalletRepository;
+use Modules\PaymentGateway\PaymentGatewayInterface;
+use Modules\PaymentGateway\Hyperfpay;
+
 return [
+	TransactionRepositoryInterface::class => TransactionRepository::class,
+	UserRepositoryInterface::class => UserRepository::class,
+	WalletRepositoryInterface::class => WalletRepository::class,
+	PaymentGatewayInterface::class => Hyperfpay::class,
 ];
