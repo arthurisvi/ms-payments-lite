0ce722cacbfe87b7a2abe398389a2acd63ef5988
arthurisvi <spfcarthur1@gmail.com>
2024-12-10 20:08:17 -0300

    chore: configuração de banco de dados
    - criação de tabelas
    - ajustes no env de exemplo

---
+++ migrations/2024_12_10_003655_create_wallets_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Hyperf\Database\Schema\Schema;
+use Hyperf\Database\Schema\Blueprint;
+use Hyperf\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+         Schema::create('wallets', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->decimal('balance', 15, 2)->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('wallets');
+    }
+};
---
+++ migrations/2024_12_10_003723_create_transactions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Hyperf\Database\Schema\Schema;
+use Hyperf\Database\Schema\Blueprint;
+use Hyperf\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('payer_id');
+            $table->uuid('payee_id');
+            $table->foreign('payer_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('payee_id')->references('id')->on('users')->onDelete('cascade');
+            $table->decimal('value', 15, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
