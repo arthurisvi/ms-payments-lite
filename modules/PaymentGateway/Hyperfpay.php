f7f11fe6d7767bc05e37e560c676400a47122c3c
arthurisvi <spfcarthur1@gmail.com>
2024-12-11 20:19:02 -0300

    feat: consulta da api do serviço autorizador

--- modules/PaymentGateway/Hyperfpay.php
+++ modules/PaymentGateway/Hyperfpay.php
@@ -2,10 +2,29 @@
 
 namespace Modules\PaymentGateway;
 
+use Hyperf\Guzzle\ClientFactory;
+
 class Hyperfpay implements PaymentGatewayInterface {
 
-	public function authorizeTransaction(): bool {
-		return true;
-	}
+	public function __construct(
+		private ClientFactory $clientFactory
+	) {}
 
+	public function authorizeTransaction(): bool {
+		$client = $this->clientFactory->create();
+
+		try {
+			$response = $client->get('https://util.devi.tools/api/v2/authorize');
+			$statusCode = $response->getStatusCode();
+
+			if ($statusCode === 200) {
+				$body = json_decode($response->getBody()->getContents(), true);
+				return isset($body['data']['authorization']) && $body['data']['authorization'] === true;
+			}
+		} catch (\Throwable $e) {
+			return false;
+		}
+
+		return false;
+	}
 }
