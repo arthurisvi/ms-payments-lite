43f1c3bd9c5e6de26be860f3e6b848a036382d9d
arthurisvi <spfcarthur1@gmail.com>
2024-12-13 01:06:15 -0300

    feat: implementação concreta da estrategia de envio de notificacao via email

--- modules/Notification/NotificationEmailStrategy.php
+++ modules/Notification/NotificationEmailStrategy.php
@@ -2,9 +2,28 @@
 
 namespace Modules\Notification;
 
+use Hyperf\Guzzle\ClientFactory;
+
 class NotificationEmailStrategy implements NotificationStrategyInterface {
 
-	public function sendNotification(NotificationDTO $notification): void {
+	public function __construct(
+		private ClientFactory $clientFactory
+	) {}
 
+	public function sendNotification(NotificationDTO $notification): void {
+		try {
+			$client = $this->clientFactory->create();
+
+			$client->post('https://util.devi.tools/api/v1/notify', [
+				'json' => [
+					'to' => $notification->recipientIdentifier,
+					'subject' => $notification->subject,
+					'message' => $notification->message
+				]
+			]);
+
+		} catch (\Throwable $e) {
+			print_r("Erro ao tentar enviar a notificação: " . $e->getMessage());
+		}
 	}
 }
