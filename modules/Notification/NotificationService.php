95e6e51a4d882793752620c0541f836fba0fb487
arthurisvi <spfcarthur1@gmail.com>
2024-12-14 18:45:20 -0300

    refactor: remove NotificationBuilder
    entende-se não haver a necessidade de utilizar o Builder Pattern, visto que o objeto criado não trata-se de um objeto complexo, estaria apenas adicionando mais uma camada desnecessária.

--- modules/Notification/NotificationService.php
+++ modules/Notification/NotificationService.php
@@ -4,6 +4,7 @@ namespace Modules\Notification;
 
 use Hyperf\Di\Annotation\Inject;
 use Modules\Notification\Template\NotificationTemplateInterface;
+use Modules\Notification\Types\NotificationDTO;
 
 class NotificationService {
 
@@ -14,12 +15,11 @@ class NotificationService {
 
 	public function send(object $context, NotificationTemplateInterface $template, array $channels): void {
 		foreach ($channels as $channel) {
-			$notification = (new NotificationBuilder())
-				->setRecipient($this->recipientResolver->resolve($channel,
-				$context->payee))
-				->setSubject($template->getSubject())
-				->setMessage($template->getMessage($context))
-				->build();
+			$notification = new NotificationDTO(
+				recipientIdentifier: $this->recipientResolver->resolve($channel, $context->payee),
+				subject: $template->getSubject(),
+				message: $template->getMessage($context)
+			);
 
 			NotificationFactory::createStrategy($channel)->sendNotification($notification);
 		}
